162e53664ac2988ed6e77d911e7733b8415a8d3f
justina177 <justinaenenche@yahoo.com>
2023-09-12 14:48:50 +0100

    adde the other route to POST UPDATE and DELETE request

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import { router as userRouter } from "./routes/userRoute.js"
 
 const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api', (req, res) => {
-    res.status(200).json({message: "Get Name"}) 
-})
+app.use('/api', (userRouter)) 
 
 app.listen(port, () => console.log(`Server Running on port ${port}`))
